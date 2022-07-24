b07013849de7aa131e71b099959664ec3a945d54
h-k-nyosu <100386872+h-k-nyosu@users.noreply.github.com>
2022-07-24 17:35:30 +0900

    packaging dezero

--- dezero/core_simple.py
+++ dezero/core_simple.py
@@ -1,12 +1,28 @@
-import unittest
 import weakref
 import contextlib
 import numpy as np
 
+# =============================================================================
+# Config
+# =============================================================================
 class Config:
     enable_backprop = True
-    
 
+@contextlib.contextmanager
+def use_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def no_grad():
+    return use_config('enable_backprop', False)
+
+# =============================================================================
+# Variable / Function
+# =============================================================================
 class Variable:
     __array_priority__ = 200
     def __init__(self, data):
@@ -19,7 +35,33 @@ class Variable:
         self.grad = None
         self.creator = None
         self.generation = 0
+        
+    @property
+    def shape(self):
+        return self.data.shape
     
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self is None:
+            return 'variable(None)'
+        else:
+            p = str(self.data).replace('\n', '\n' + ' ' * 9)
+            return 'variable(' + p + ')'
+        
     def set_creator(self, func):
         self.creator = func
         self.generation = func.generation + 1
@@ -30,7 +72,6 @@ class Variable:
     def backward(self, retain_grad=False):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
-    
             
         funcs = []
         seen_set = set()
@@ -62,31 +103,6 @@ class Variable:
             if not retain_grad:
                 for y in f.outputs:
                     y().grad = None
-    @property
-    def shape(self):
-        return self.data.shape
-    
-    @property
-    def ndim(self):
-        return self.data.ndim
-    
-    @property
-    def size(self):
-        return self.data.size
-    
-    @property
-    def dtype(self):
-        return self.data.dtype
-    
-    def __len__(self):
-        return len(self.data)
-    
-    def __repr__(self):
-        if self is None:
-            return 'variable(None)'
-        else:
-            p = str(self.data).replace('\n', '\n' + ' ' * 9)
-            return 'variable(' + p + ')'
 
 def as_array(x):
     if np.isscalar(x):
@@ -123,23 +139,10 @@ class Function:
     def backward(self, gy):
         raise NotImplementedError()
 
-class Square(Function):
-    def forward(self, x):
-        return x ** 2
-    
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
-        return gx
 
-class Exp(Function):
-    def forward(self, x):
-        return np.exp(x)
-    
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = np.exp(x) * gy
-        return gx
+# =============================================================================
+# 四則演算 / 演算子のオーバーロード
+# =============================================================================
 
 class Add(Function):
     def forward(self, x0, x1):
@@ -196,14 +199,6 @@ class Pow(Function):
         gx = c * x ** (c - 1) * gy
         return gx
 
-def square(x):
-    f = Square()
-    return f(x)
-
-def exp(x):
-    f = Exp()
-    return f(x)
-
 def add(x0, x1):
     x1 = as_array(x1)
     return Add()(x0, x1)
@@ -234,73 +229,15 @@ def pow(x, c):
 def neg(x):
     return Neg()(x)
 
-Variable.__mul__ = mul
-Variable.__rmul__ = mul
-Variable.__add__ = add
-Variable.__radd__ = add
-Variable.__sub__ = sub
-Variable.__rsub__ = rsub
-Variable.__truediv__ = div
-Variable.__rtruediv__ = rdiv
-Variable.__neg__ = neg
-Variable.__pow__ = pow
+def setup_variable():
+    Variable.__mul__ = mul
+    Variable.__rmul__ = mul
+    Variable.__add__ = add
+    Variable.__radd__ = add
+    Variable.__sub__ = sub
+    Variable.__rsub__ = rsub
+    Variable.__truediv__ = div
+    Variable.__rtruediv__ = rdiv
+    Variable.__neg__ = neg
+    Variable.__pow__ = pow
 
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(x.data - eps)
-    x1 = Variable(x.data + eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
-    
-
-@contextlib.contextmanager
-def use_config(name, value):
-    old_value = getattr(Config, name)
-    setattr(Config, name, value)
-    try:
-        yield
-    finally:
-        setattr(Config, name, old_value)
-
-def no_grad():
-    return use_config('enable_backprop', False)
-
-
-class SquareTest(unittest.TestCase):
-    def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)
-    
-    def test_backward(self):
-        x = Variable(np.array(3.0))
-        y = square(x)
-        y.backward()
-        expected = np.array(6.0)
-        self.assertEqual(x.grad, expected)
-    
-    def test_gradient_check(self):
-        x = Variable(np.random.rand(1))
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)
-        self.assertTrue(flg)
-
-
-x = Variable(np.array([3.0]))
-
-y1 = 6 / x
-y2 = x / 6
-y3 = 10 - x
-y4 = x - 10
-y5 = x ** 4
-z = 5 * -x / x ** 2 - 1
-
-print(y1)
-print(y2)
-print(y3)
-print(y4)
-print(y5)
-print(z)
